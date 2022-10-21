ed79dee25f706ca848181b7c3b7ebceb83cf6d20
ISHARMA13 <93185715+ISHARMA13@users.noreply.github.com>
2022-10-21 11:10:28 +0530

    foreign keys added

--- create.sql
+++ create.sql
@@ -2,34 +2,32 @@ CREATE USER 'ishifree'@'localhost' IDENTIFIED BY 'Freelancer@9876';
 CREATE DATABASE 'ishifree';
 grant all permissions on 'ishifree' to 'freelancers'@'localhost';
 
--- 2 users or  one user?? freelancers and recruiters
--- or 2 data bases?
+-- Freelancers
 
--- messenger system
--- updater system
-
--- Tables:
--- userinfo, applications, jobs_applied 
--- recruiterinfo, jobs posted - when, what, till, applications recived per job posted
-
-create table 'freelancers' (Id varchar(20) primary KEY,
-password varchar(20) not null);
+create table freelancers (Id varchar(20) primary key,
+password varchar(20) not null
+);
 
 create table freewallet (Id varchar(20) primary key,
-amount int default 0, check (amount>0));
+amount int default 0, check (amount>0),
+foreign key(Id) references freelancers(Id)
+);
 
 create table freedetails (Id varchar(20) primary key,
 name varchar(30) not null,
 photo_url varchar(100),
 phone_no varchar(25) not null, 
-address varchar(100));
+address varchar(100),
+foreign key(Id) references freelancers(Id)
+);
 
 create table freeeducation (Id varchar(20),
 institute varchar(30) not null,
 degree varchar(10),
 year_start int not null, 
 year_end int,
-primary key(Id,year_start)
+primary key(Id, institute, year_start),
+foreign key(Id) references freelancers(Id)
 );
 
 create table freeexperience (Id varchar(20),
@@ -37,16 +35,19 @@ company varchar(30) not null,
 title varchar(10),
 start_date date not null, 
 end_date date,
-primary key(Id,start_date)
+primary key(Id, company, title, start_date)
+foreign key(Id) references freelancers(Id)
 );
 
+-- employer
 
-
-create table 'employer' (Id varchar(20) primary KEY,
-password varchar(20) not null);
+create table employer (Id varchar(20) primary key,
+password varchar(20) not null
+);
 
 create table empwallet (Id varchar(20) primary key,
-amount int default 0);
+amount int default 0
+);
 
 create table empdetails (Id varchar(20) primary key,
 name varchar(30) not null,
@@ -54,7 +55,8 @@ photo_url varchar(100),
 position varchar(100),
 company varchar(100),
 phone_no varchar(25) not null, 
-address varchar(100));
+address varchar(100)
+);
 
 
 create table jobs (job_id varchar(20),
@@ -67,7 +69,8 @@ skill varchar(60) not null,
 description text(800) not null,
 post_date date not null, 
 exp_date date,
-primary key(job_id)
+primary key(job_id),
+foreign key(emp_id) references employer(Id)
 );
 
 create table proposals (
@@ -81,4 +84,7 @@ budget int not null,
 coverletter text(800) not null,
 post_date date not null, 
 primary key(proposal_id)
+foreign key(emp_id) references employer(Id),
+foreign key(free_id) references freelancers(Id),
+foreign key(job_id) references jobs(job_id)
 );
